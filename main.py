3645082012d841e2a69356b9000633eaf566b9e3
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2024-12-25 19:07:09 -0500

    AHH IT WORKS

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+from pygame.locals import *
+
+#Define Constatnts
+MOUSE_SIZE = (150, 150)
+
+pygame.init()
+screen = pygame.display.set_mode((626, 626), RESIZABLE)
+pygame.display.set_caption("Shooter")
+bgImg = pygame.image.load('assets/bg.png')
+
+#Custom cursor settings
+pygame.mouse.set_visible(False)
+cursorImg = pygame.image.load('assets/rifle.png')
+cursorImg = pygame.transform.scale(cursorImg, MOUSE_SIZE)
+
+gameRunning = True
+while gameRunning:
+    screen.blit(bgImg, (0, 0))
+    
+    mousePos = pygame.mouse.get_pos()
+    screen.blit(cursorImg, mousePos)
+    
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            gameRunning = False
+    
+    pygame.display.update()
+    
+    
+exit()
